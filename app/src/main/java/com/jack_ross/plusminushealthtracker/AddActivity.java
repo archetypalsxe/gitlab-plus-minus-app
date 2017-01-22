b874eac7522deeb77753368d175211b12075ed19
Jack Ross, III <mrjackross@gmail.com>
2017-01-22 12:54:07 -0500

    Now saving actual data rather than dummy data. Fixed date and time display after saving activities. Now displaying current and previous 3 day's sums of weights

--- app/src/main/java/com/jack_ross/plusminushealthtracker/AddActivity.java
+++ app/src/main/java/com/jack_ross/plusminushealthtracker/AddActivity.java
@@ -8,8 +8,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 
@@ -30,7 +33,7 @@ public class AddActivity extends AppCompatActivity {
      */
     public void saveActivity(View view) {
         DatabaseHelper dbHelper = new DatabaseHelper(getApplicationContext());
-        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
 
         ContentValues values = new ContentValues();
 
@@ -40,8 +43,8 @@ public class AddActivity extends AppCompatActivity {
                 tsLong.toString()
         );
         values.put("userId", 1);
-        values.put("description", "Testing to see if this works");
-        values.put("weight", -2);
+        values.put("description", this.getDescription());
+        values.put("weight", this.getWeight());
 
         long rowId;
         rowId = database.insert("activities", null, values);
@@ -59,29 +62,128 @@ public class AddActivity extends AppCompatActivity {
                         "description",
                         "weight"
                 },
-                "userId = ?",
-                new String[]{"1"},
+                "userId = ? AND dateTime >= ?",
+                new String[]{
+                        "1",
+                        this.getUnixTimeStamp(0, true)
+                },
                 null,
                 null,
                 "dateTime DESC"
         );
 
         String activities = "";
+        int total = 0;
         while (cursor.moveToNext()) {
+            total += Integer.parseInt(cursor.getString(cursor.getColumnIndexOrThrow("weight")));
             String date = this.convertTimestamp(cursor.getString(cursor.getColumnIndexOrThrow("dateTime")));
-            activities +=  date + ": ";
+            activities += date + ": ";
             activities += cursor.getString(cursor.getColumnIndexOrThrow("weight")) + " ";
-            activities += cursor.getString(cursor.getColumnIndexOrThrow("description")) + "\n";
+            activities += cursor.getString(cursor.getColumnIndexOrThrow("description")) + "<br/>";
         }
 
+        activities = "<b>Daily Total: "+ String.valueOf(total) +"</b><br/>"+ activities;
+        activities += "<b>Yesterday's Total: "+ this.getDaysTotal(-1, database);
+        activities += "<br/>Two Days Ago's Total: "+ this.getDaysTotal(-2, database);
+        activities += "<br/>Three Days Ago's Total: "+ this.getDaysTotal(-3, database) +"</b>";
         intent.putExtra(EXTRA_MESSAGE, activities);
 
         startActivity(intent);
     }
 
+    /**
+     * Converts a provided timestamp into a display for users
+     *
+     * @param timeStamp String
+     * @return String
+     */
     protected String convertTimestamp(String timeStamp) {
-        Date date = new Date(Integer.parseInt(timeStamp) * 1000);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM d, yyyy 'at' h:mm a");
+        Date date = new Date(Integer.parseInt(timeStamp) * 1000L);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy 'at' h:mm a");
         return dateFormat.format(date);
+    }
+
+    /**
+     * Retrieves the description of the activity from the add activity page
+     *
+     * @return String
+     */
+    protected String getDescription() {
+        EditText editText = (EditText) findViewById(R.id.activityDescription);
+        return editText.getText().toString();
+    }
+
+    /**
+     * Returns the weight of the activity from the add activity page
+     *
+     * @return int
+     */
+    protected int getWeight() {
+        RadioGroup radioGroup = (RadioGroup)findViewById(R.id.activityWeight);
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        RadioButton selectedButton = (RadioButton) findViewById(radioId);
+        return Integer.parseInt(selectedButton.getText().toString());
+    }
+
+    /**
+     * Gets a unix timestamp of the start of the current day
+     *
+     * @param dayDifference int
+     * @param startOfDay boolean
+     * @return String
+     */
+    protected String getUnixTimeStamp(int dayDifference, boolean startOfDay) {
+        Calendar calendar = Calendar.getInstance();
+        if(!startOfDay) {
+            dayDifference++;
+        }
+        calendar.add(Calendar.DAY_OF_MONTH, dayDifference);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        if(!startOfDay) {
+            calendar.add(Calendar.SECOND, -1);
+        }
+        /*
+            Log.d("Debug", "Day Difference: "+ String.valueOf(dayDifference));
+            Log.d("Debug", "Start Of Day"+ String.valueOf(startOfDay));
+            SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy h:mm a");
+            Log.d("Debug", sdf.format(calendar.getTime()));
+         */
+        return String.valueOf(calendar.getTimeInMillis() / 1000);
+    }
+
+    /**
+     * Returns the activity total from yesterday
+     *
+     * @param daysDifference int
+     * @param database Database
+     * @return String
+     */
+    protected String getDaysTotal(int daysDifference, SQLiteDatabase database) {
+        Cursor cursor = database.query(
+                "activities",
+                new String[]{
+                        "SUM(weight) weight"
+                },
+                "userId = ? AND dateTime BETWEEN ? AND ?",
+                new String[]{
+                        "1",
+                        this.getUnixTimeStamp(daysDifference, true),
+                        this.getUnixTimeStamp(daysDifference, false)
+                },
+                null,
+                null,
+                "dateTime DESC"
+        );
+        if(cursor.moveToFirst() && cursor.getCount() > 0) {
+            String weight = cursor.getString(cursor.getColumnIndexOrThrow("weight"));
+            if(weight != null) {
+                return weight;
+            }
+        }
+
+        return "No activities";
     }
 }
--- app/src/main/java/com/jack_ross/plusminushealthtracker/DisplayMessageActivity.java
+++ app/src/main/java/com/jack_ross/plusminushealthtracker/DisplayMessageActivity.java
@@ -3,6 +3,7 @@ package com.jack_ross.plusminushealthtracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -18,7 +19,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = new TextView(this);
         TextView text = (TextView) findViewById(0);
-        textView.setText(message);
+        textView.setText(Html.fromHtml(message));
         textView.setMovementMethod(new ScrollingMovementMethod());
 
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
